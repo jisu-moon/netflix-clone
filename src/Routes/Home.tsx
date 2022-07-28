91ef764cde9354a252bf5657985ff03bd5be8a75
Jisu Moon <answltn5935@gmail.com>
2022-07-28 16:45:50 +0900

    home main banner done

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -1,10 +1,74 @@
 import { useQuery } from '@tanstack/react-query';
-import { getMovies } from '../apis';
+import styled from 'styled-components';
+import { getMovies, IGetMovies } from '../apis';
+import { makeImagePath } from '../utils';
+
+const Wrapper = styled.div`
+  width: 100vw;
+`;
+const Loading = styled.div`
+  width: 100vw;
+  height: calc(100vw - 70px);
+  position: fixed;
+  top: 70px;
+  left: 0;
+  background: #000;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const Banner = styled.div<{ bgPath: string }>`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding: 60px;
+  background: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 1)),
+    url(${props => props.bgPath});
+  background-size: cover;
+`;
+const BannerTitle = styled.h2`
+  font-size: 64px;
+  font-weight: bold;
+  margin-bottom: 20px;
+  color: #fff;
+`;
+const BannerOverview = styled.p`
+  font-size: 24px;
+  width: 40%;
+  line-height: 1.3;
+  padding-left: 2px;
+  word-break: keep-all;
+  color: #fff;
+`;
 
 function Home() {
-  const { data, isLoading } = useQuery(['movies', 'nowPlaying'], getMovies);
-  console.log(data, isLoading);
-  return <div style={{ height: '500vh' }}>Home</div>;
+  const { data, isLoading } = useQuery<IGetMovies>(
+    ['movies', 'nowPlaying'],
+    getMovies,
+  );
+  const randomRank = Math.floor(Math.random() * 5);
+  console.log(data, isLoading, randomRank);
+  return (
+    <Wrapper>
+      {isLoading ? (
+        <Loading>Loading...</Loading>
+      ) : (
+        <>
+          <Banner
+            bgPath={makeImagePath(
+              data?.results[randomRank].backdrop_path || '',
+            )}
+          >
+            <BannerTitle>{data?.results[randomRank].title}</BannerTitle>
+            <BannerOverview>
+              {data?.results[randomRank].overview}
+            </BannerOverview>
+          </Banner>
+        </>
+      )}
+    </Wrapper>
+  );
 }
 
 export default Home;
--- src/apis.ts
+++ src/apis.ts
@@ -3,10 +3,29 @@ import axios from 'axios';
 const API_KEY = '9a11d3415c984ae77df020c63907ea0e';
 const BASE_URL = 'https://api.themoviedb.org/3';
 
-export function getMovies() {
+interface IMoviesResults {
+  backdrop_path: string;
+  id: number;
+  overview: string;
+  poster_path: string;
+  release_date: string;
+  title: string;
+}
+export interface IGetMovies {
+  page: number;
+  results: IMoviesResults[];
+  dates: {
+    maximum: string;
+    minimum: string;
+  };
+  total_pages: number;
+  total_results: number;
+}
+
+export const getMovies = () => {
   return axios
     .get(
       `${BASE_URL}/movie/now_playing?api_key=${API_KEY}&language=ko-KR&page=1&region=kr`,
     )
     .then(res => res.data);
-}
+};
